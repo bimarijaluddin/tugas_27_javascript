d064cded395a9017fbcf0dcdb9496cd4adcb0695
bima rijaluddin <bimarijal@gmail.com>
2019-09-20 14:27:49 +0800

    Tugas 27 Javascript
    Tugas dari niomic

---
+++ tugas27.js
@@ -0,0 +1,13 @@
+// "No”, ”rumah saya”,30, “A”;
+var kata1 = "No";
+var kata2 = "rumah saya";
+var kata3 = 30;
+var kata4 = "A";
+
+console.log(kata1 + " " + kata2 + " " + String(kata3) + kata4);
+
+// “Jalan pangeran Tirtayasa No”, 70
+var kata5 = "Jalan";
+var kata6 = "pangeran Tirtayasa No"
+var kata7 = 70;
+console.log(kata5 + " " + kata6 + " " + kata7);
